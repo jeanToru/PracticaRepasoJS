efa23a4ad3646a71ce901f343851c0f8c9d88396
Jean Carlo <jeantoruno00@gmail.com>
2020-07-13 12:13:15 -0600

    Ejercicio 6 Correcto

--- JS1-6/script.js
+++ JS1-6/script.js
@@ -2,9 +2,9 @@ let numero = parseFloat(prompt('Digite un número: '));
 
 let mensaje = '';
 
-if (numero % 1 == 0 || numero % numero == 0) {
-    mensaje = 'El número es primo';
-}else{
+if (numero < 2 || (numero % 2 == 0 && numero != 2)) {
     mensaje = 'El número no es primo';
+} else {
+    mensaje = 'El número es primo';
 }
 console.log(mensaje);
