50fb0dbbff4d2f2865d831f03b926ab3d1d028b2
Jean Carlo <jeantoruno00@gmail.com>
2020-07-25 16:47:02 -0600

    Ejercicio 1 modificado

--- JS1-1/script.js
+++ JS1-1/script.js
@@ -1,6 +1,6 @@
 let ano= parseInt(prompt("Escriba un año"));
 
-if (ano % 4 == 0 && ano % 100 != 0) {
+if (ano % 4 == 0 && ano % 100 != 0 || ano % 400 == 0) {
     console.log("El año es bisiesto");
 } else {
     console.log("El año no es bisiesto");
